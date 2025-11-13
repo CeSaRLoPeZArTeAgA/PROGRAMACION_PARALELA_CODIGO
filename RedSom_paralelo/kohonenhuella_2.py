98c9ac86491d37bff3037e09d8c6df6e55bffedf
CeSaRLoPeZArTeAgA <clopeza@uni.pe>
2025-11-13 10:06:19 -0500

    RedSom

---
+++ RedSom_paralelo/kohonenhuella_2.py
@@ -0,0 +1,171 @@
+import numpy as np
+import math
+from PIL import Image
+# datos globales
+LEFT_LOOP   = 1
+RIGHT_LOOP  = 2
+WHORL       = 3
+ARCO        = 4
+vecindad_gaussiana = np.array(
+    [[0,0,0.125,0,0],
+     [0,0.25,0.5,0.25,0],
+     [0.125,0.5,1,0.5,0.125],
+     [0,0.25,0.5,0.25,0],
+     [0,0,0.125,0,0]]
+    ,np.float32)
+
+# crear una matriz de pesos sinapticos
+W = np.zeros([324,14,14])
+for i in range(324):
+    W[i,2:12,2:12] = np.random.rand(10,10)
+# Crear una matriz temporal
+M_t = np.zeros([14,14],np.float32)
+# crear la matriz de indices o resultante de lo aprendido
+M_index = np.zeros([10,10],np.float32)
+max_iter = 500# maximo cantidad de generaciones
+alpha_max = 0.45#factor de aprendizaje inicial
+
+def sobel(patron):
+    m,n = patron.shape
+    I = np.array(patron,np.float32)
+    Gx = np.zeros([m-2,n-2],np.float32)
+    Gy = np.zeros([m-2,n-2],np.float32)
+    gx = [[-1,0,1],[-2,0,2],[-1,0,1]]
+    gy = [[1,2,1],[0,0,0],[-1,-2,-1]]
+    for i in range(1,m-2):
+        for j in range(1,n-2):
+            Gx[i-1,j-1] = sum(sum(I[i-1:i+2,j-1:j+2]*gx))
+            Gy[i-1,j-1] = sum(sum(I[i-1:i+2,j-1:j+2]*gy))
+    return Gx,Gy
+def medfilt2(G,d=3):
+    m,n = G.shape
+    temp = np.zeros([m+2*(d//2),n+2*(d//2)],np.float32)
+    salida = np.zeros([m,n],np.float32)
+    temp[1:m+1,1:n+1] = G
+    for i in range(1,m):
+        for j in range(1,n):
+            A = np.asarray(temp[i-1:i+2,j-1:j+2]).reshape(-1)
+            salida[i-1,j-1] = np.sort(A)[d+1]
+    return salida
+    
+def orientacion(patron,w):
+    Gx,Gy = sobel(patron)
+    Gx = medfilt2(np.array(Gx,np.float32))
+    Gy = medfilt2(np.array(Gy,np.float32))
+    m,n = Gx.shape
+    mOrientaciones = np.zeros([m//w,n//w],np.float32)
+    for i in range(m//w):
+        for j in range(n//w):
+            YY = sum(sum(2*Gx[i*w:(i+1)*w,0:1]*Gy[i*w:(i+1)*w,0:1]))
+            XX = sum(sum(Gx[i*w:(i+1)*w,0:1]**2-Gy[i*w:(i+1)*w,0:1]**2))
+            mOrientaciones[i,j] = (0.5*math.atan2(YY,XX) + math.pi/2.0)*(18.0/math.pi)
+    return mOrientaciones
+                         
+def kohonen_salidas(E,W):# E: representativo,W: pesos
+    M = np.zeros([14,14],np.float32)
+    m = 14
+    for x in range(2,m-2):
+        for y in range(2,m-2):
+            w = W[:,x,y]
+            num = sum(E*w)
+            denom = math.sqrt(sum(E**2)*sum(w**2))
+            if num==0 or denom==0:
+                M[x,y] = 0.0
+            else:
+                M[x,y] = num/denom
+    return M
+def kohonen_reforzamiento(M1,M_t,alpha_max,E,W):
+    (yy,xx) = np.unravel_index(np.argmax(M1,axis=None),M1.shape)
+    #print(yy,xx)
+    #print(M1[yy,xx])
+    t = M_t[xx+2,yy+2] + 1
+    M_t[xx+2,yy+2] = t
+    alpha = alpha_max/t
+    i = 0
+    for x in range(xx-1,xx+3):
+        j = 0
+        for y in range(yy-1,yy-3):
+            w = W[:,x,y]
+            vg = vecindad_gaussiana[i,j]
+            d = E.T - w
+            W[:,x,y] = w + alpha*d*vg# actualizando los pesos sinapticos
+            j = j + 1
+        i = i + 1
+    return M_t,W,xx-2,yy-2
+        
+    
+def representativo(archivo):
+    im = Image.open(archivo)
+    m,n = im.size# 256x256
+    imarray = np.array(im,np.float32)# convertir de imagen a numpy
+    patron = imarray[1:m-1,1:n-1]# 254x254
+    EE = orientacion(patron,14)# 18x18
+    return np.asarray(EE).reshape(-1)# 1x324
+
+def mapear(archivo,W,M_index):
+    E = representativo(archivo)
+    M = kohonen_salidas(E,W)
+    (yy,xx) = np.unravel_index(np.argmax(M,axis=None),M.shape)
+    x = xx - 2
+    y = yy - 2
+    tipo = M_index[x,y]
+    print(x,y,tipo)
+    if tipo==LEFT_LOOP:
+        print("LEFT_LOOP")
+    if tipo==RIGHT_LOOP:
+        print("RIGHT_LOOP")
+    if tipo==WHORL:
+        print("WHORL")
+    if tipo==ARCO:
+        print("ARCO")
+        
+
+print("---------------------")
+print("creando los patrones de entrada")
+EEE = np.zeros([15,324],np.float32)
+EEE[0,:] = representativo('arco1.tif')
+EEE[1,:] = representativo('whorl1.tif')
+EEE[2,:] = representativo('right1.tif')
+EEE[3,:] = representativo('left1.tif')
+EEE[4,:] = representativo('arco2.tif')
+EEE[5,:] = representativo('whorl2.tif')
+EEE[6,:] = representativo('right2.tif')
+EEE[7,:] = representativo('left2.tif')
+EEE[8,:] = representativo('arco3.tif')
+EEE[9,:] = representativo('whorl3.tif')
+EEE[10,:] = representativo('right3.tif')
+EEE[11,:] = representativo('left3.tif')
+EEE[12,:] = representativo('arco4.tif')
+EEE[13,:] = representativo('whorl4.tif')
+EEE[14,:] = representativo('right4.tif')
+# Inicia el proceso de entrenamiento de la RN
+print("---------------------")
+print("iniciando el aprendizaje")
+for iter in range(max_iter):
+    print(iter)
+    for i in range(15):
+        E = EEE[i,:]
+        M = kohonen_salidas(E,W)
+        M_t,W,x,y = kohonen_reforzamiento(M,M_t,alpha_max,EEE[0,:],W)
+        if i in [0,4,8,12]:
+            M_index[x,y] = ARCO
+        elif i in [3,7,11]:
+            M_index[x,y] = LEFT_LOOP
+        elif i in [2,6,10,14]:
+            M_index[x,y] = RIGHT_LOOP
+        elif i in [1,5,9,13]:
+            M_index[x,y] = WHORL
+print("Fin del aprendizaje")
+print("---------------------")
+print("Matriz de indices o representacion de lo aprendido: ")
+print(M_index)
+print("---------------------")
+print("Realizar el mapeo o comprobación: ")
+while True:
+    mifile = input ("Ingrese el nombre del file:")
+    mapear(mifile+'.tif',W,M_index)
+##    mapear('arco1.tif',W,M_index)
+    
+#print(EEE)
+#print(EEE.shape)
+    
